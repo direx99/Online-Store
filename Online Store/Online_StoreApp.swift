3300cdbe98ddf54c66096d59c5439d3e8f8eeac9
Dinith Hasaranga <dinith@team.bhasha.lk>
2023-12-03 09:01:43 +0530

    Initial Commit

---
+++ Online Store/Online_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_StoreApp.swift
+//  Online Store
+//
+//  Created by Dinith Hasaranga on 2023-12-03.
+//
+
+import SwiftUI
+
+@main
+struct Online_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
